84afe2179b71fedc4285cb09a8a3337de5b37f71
Indra Muliana <indra.ndra26@gmail.com>
2022-05-22 09:58:52 +0700

    Add pinia persisted state

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import "./bootstrap";
 import AppTemplate from "./layouts/AppTemplate.vue";
 import router from './router';
 import { createPinia } from 'pinia'
+import persitedState from './plugin/persistedState'
 
 // Global Components
 import Alert from "./components/Alert.vue";
@@ -11,15 +12,17 @@ import SubmitButton from "./components/SubmitButton.vue";
 import ResetButton from "./components/ResetButton.vue";
 
 const app = createApp(AppTemplate);
+const store = createPinia();
+store.use(persitedState);
 
 // Registering Global Component
 app.component("Alert", Alert)
     .component("SubmitButton", SubmitButton)
     .component("ResetButton", ResetButton);
 
-// Registering Router
+// Registering App Plugin
 app.use(router);
-app.use(createPinia());
+app.use(store);
 
 // Mounting app
 app.mount('#app');
---
+++ resources/js/plugin/persistedState.js
@@ -0,0 +1,15 @@
+import createPersistedState from "pinia-persistedstate";
+import SecureLS from "secure-ls";
+
+const ls = new SecureLS({ isCompression: false });
+const persitedState = createPersistedState({
+    key: 'app-data-store',
+    paths: ['authDataStore'],  // Keep state, use module id, or state
+    storage: {
+        getItem: (key) => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: (key) => ls.remove(key)
+    }
+});
+
+export default persitedState;
