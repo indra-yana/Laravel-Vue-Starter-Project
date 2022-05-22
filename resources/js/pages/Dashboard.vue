39d7333e34adccedad9796cae61935761f363add
Indra Muliana <indra.ndra26@gmail.com>
2022-05-22 20:52:38 +0700

    Update

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-        <h5>Dashboard</h5>
-        <p>Welcome to Dahboard Page</p>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="p-5 mb-4 bg-primary-soft rounded-3 border ">
+                <div class="container-fluid py-5">
+                    <h1 class="display-5 fw-bold">Overview</h1>
+                    <p class="col-md-8 fs-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <button class="btn btn-primary btn-lg" type="button">Get Started</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
